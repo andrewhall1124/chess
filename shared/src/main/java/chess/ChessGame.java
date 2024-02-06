e3bbe3082cc4bd20fa07b96edf48d1f2accc3f16
Andrew Hall <andrewmartinhall2@gmail.com>
2024-02-06 11:44:08 -0700

    Starting from scratch

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -64,41 +64,8 @@ public class ChessGame {
      * startPosition
      */
     public HashSet<ChessMove> validMoves(ChessPosition startPosition) {
-        if(board.getPiece(startPosition) == null){
-            return null;
-        } else{
-            ChessPiece originalPiece = board.getPiece(startPosition);
-            HashSet<ChessMove> possibleMoves = originalPiece.pieceMoves(board,startPosition);
-            HashSet<ChessMove> validatedMoves = new HashSet<>();
-            for(ChessMove move : possibleMoves){
-                ChessPiece promotionPiece = new ChessPiece(this.teamTurn,originalPiece.getPieceType());
-                if(move.getPromotionPiece() != null){
-                    promotionPiece.setPieceType(move.getPromotionPiece());
-                }
-                //Make move
-                if(move.getPromotionPiece() != null){
-                    board.removePiece(move.getStartPosition());
-                    board.addPiece(move.getEndPosition(),promotionPiece);
-                }
-                else{
-                    board.removePiece(move.getStartPosition());
-                    board.addPiece(move.getEndPosition(),originalPiece);
-                }
-                if(isInCheck(originalPiece.getTeamColor())){
-                    if(move.getPromotionPiece() != null){
-                        board.removePiece(move.getEndPosition());
-                        board.addPiece(move.getStartPosition(),promotionPiece);
-                    } else{
-                        board.removePiece(move.getEndPosition());
-                        board.addPiece(move.getStartPosition(),originalPiece);
-                    }
-                }
-                else{
-                    validatedMoves.add(move);
-                }
-            }
-            return validatedMoves;
-        }
+        return new HashSet<>();
+
     }
 
     /**
@@ -108,50 +75,7 @@ public class ChessGame {
      * @throws InvalidMoveException if move is invalid
      */
     public void makeMove(ChessMove move) throws InvalidMoveException {
-        if(teamTurn == board.getPiece(move.getStartPosition()).getTeamColor()){
-            //Figure out end piece
-            ChessPiece originalPiece = board.getPiece(move.getStartPosition());
-            ChessPiece promotionPiece = new ChessPiece(this.teamTurn,originalPiece.getPieceType());
-            if(move.getPromotionPiece() != null){
-               promotionPiece.setPieceType(move.getPromotionPiece());
-            }
-            HashSet<ChessMove> possibleMoves = originalPiece.pieceMoves(board,move.getStartPosition());
-            //Check that move is possible
-            if(possibleMoves.contains(move)){
-                //Make move
-                if(move.getPromotionPiece() != null){
-                    board.removePiece(move.getStartPosition());
-                    board.addPiece(move.getEndPosition(),promotionPiece);
-                } else{
-                    board.removePiece(move.getStartPosition());
-                    board.addPiece(move.getEndPosition(),originalPiece);
-                }
-                //Check if King is in check
-                if(isInCheck(this.teamTurn)){
-                    if(move.getPromotionPiece() != null){
-                        board.removePiece(move.getEndPosition());
-                        board.addPiece(move.getStartPosition(),promotionPiece);
-                    } else{
-                        board.removePiece(move.getEndPosition());
-                        board.addPiece(move.getStartPosition(),originalPiece);
-                    }
-                    throw new InvalidMoveException("Invalid move: puts king in check");
-                }
-                //Alternate team turn
-                if(this.teamTurn == TeamColor.WHITE){
-                    this.teamTurn = TeamColor.BLACK;
-                }
-                else{
-                    this.teamTurn = TeamColor.WHITE;
-                }
-            }
-            else{
-                throw new InvalidMoveException("Invalid move: end position not allowed");
-            }
-        }
-        else{
-            throw new InvalidMoveException("Invalid move: not your turn");
-        }
+        throw new InvalidMoveException("Not implemented");
     }
 
     /**
@@ -161,23 +85,7 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        ChessPosition kingPosition = getKingPosition(teamColor);
-        //Check all possible moves
-        for(int i = 1; i <= 8; i++){
-            for(int j = 1; j <= 8; j++){
-                ChessPosition currentPosition = new ChessPosition(i,j);
-                if(board.getPiece(currentPosition) != null
-                        && board.getPiece(currentPosition).getTeamColor() != teamColor){
-                    HashSet<ChessMove> possibleMoves = board.getPiece(currentPosition).pieceMoves(board,currentPosition);
-                    for(ChessMove move : possibleMoves){
-                        if(move.getEndPosition().equals(kingPosition)){
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
-        return false;
+        return true;
     }
 
     /**
@@ -187,22 +95,7 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        //Iterate through whole board
-        for(int i = 1; i <= 8; i++){
-            for(int j = 1; j <= 8; j++){
-                ChessPosition currentPosition = new ChessPosition(i,j);
-                if(board.getPiece(currentPosition) != null){
-                    ChessPiece currentPiece = board.getPiece(currentPosition);
-                    if(currentPiece.getTeamColor() == teamColor){
-                        HashSet<ChessMove> validMoves = validMoves(currentPosition);
-                        if(!validMoves.isEmpty()){
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-        return true;
+       return true;
     }
 
     /**
