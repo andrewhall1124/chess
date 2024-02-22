061854a212ec302fd74e5043f18db2fc4e9e33f8
Andrew Hall <andrewmartinhall2@gmail.com>
2024-02-21 21:12:10 -0700

    Initialized phase 3

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,11 @@
+package model;
+
+import chess.ChessGame;
+
+public class GameData {
+    private Number id;
+    private String whiteUserName;
+    private String blackUserName;
+    private String gameName;
+    private ChessGame game;
+}
