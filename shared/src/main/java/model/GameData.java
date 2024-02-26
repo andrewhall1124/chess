27df099287ac03e6d0ed129ba39e8a7c832196db
Andrew Hall <andrewmartinhall2@gmail.com>
2024-02-26 10:27:17 -0700

    Created record classes

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -2,42 +2,5 @@ package model;
 
 import chess.ChessGame;
 
-public class GameData {
-    private String id = "";
-    private String whiteUserName = "";
-    private String blackUserName = "";
-    private final String gameName;
-    private ChessGame game;
-
-    public GameData(String gameName){
-        this.gameName = gameName;
-    }
-
-    public void setWhiteUserName(String name){
-        this.whiteUserName = name;
-    }
-
-    public void setBlackUserName(String name){
-        this.blackUserName = name;
-    }
-
-    public void setId(String uuid){
-        this.id = uuid;
-    }
-
-    public String getId(){
-        return this.id;
-    }
-
-    public String getName(){
-        return this.gameName;
-    }
-
-    public String getWhiteUserName(){
-        return this.whiteUserName;
-    }
-
-    public String getBlackUserName(){
-        return this.blackUserName;
-    }
+public record GameData(String gameID, String whiteUserName, String blackUserName, String gameName, ChessGame game) {
 }
