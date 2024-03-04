83709682b14816ba0f99526e9cb38070e4d448fe
Andrew Hall <andrewmartinhall2@gmail.com>
2024-03-04 11:18:42 -0700

    Initialized for phase 4

---
+++ server/src/main/java/dataAccess/SQLUserDAO.java
@@ -0,0 +1,30 @@
+package dataAccess;
+
+public class SQLUserDAO {
+    public SQLUserDAO() throws Exception {
+        configureDatabase();
+    }
+    private void configureDatabase() throws Exception {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        }
+    }
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  user (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `name` varchar(256) NOT NULL,
+              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(type),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+}
