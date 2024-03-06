89b95fc8186d07cfdac7a210463d401477c1a3c7
Andrew Hall <andrewmartinhall2@gmail.com>
2024-03-05 18:35:41 -0700

    All positive tests

---
+++ server/src/test/java/dataAccessTests/MyDataAccessTest.java
@@ -0,0 +1,125 @@
+package dataAccessTests;
+
+import chess.ChessGame;
+import dataAccess.DataAccessException;
+import dataAccess.SQLAuthDAO;
+import dataAccess.SQLGameDAO;
+import dataAccess.SQLUserDAO;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyDataAccessTest {
+    private final SQLUserDAO userDAO = new SQLUserDAO();
+    private final SQLGameDAO gameDAO = new SQLGameDAO();
+    private final SQLAuthDAO authDAO = new SQLAuthDAO();
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    UserData userData = new UserData("andrew", "password", "andrew@me.com");
+    UserData hashedUserData = new UserData("andrew", encoder.encode("password"),"andrew@me.com");
+    GameData gameOne = new GameData(1,"white","black","chess game", new ChessGame());
+    GameData gameTwo = new GameData(2,"white","black","chess game", new ChessGame());
+    GameData gameThree = new GameData(3,"white","black","chess game", new ChessGame());
+
+
+    @BeforeEach
+    public void setup() throws DataAccessException{
+        userDAO.deleteAllUsers();
+        gameDAO.deleteAllGames();
+        authDAO.deleteAllAuth();
+    }
+
+    @Test
+    public void clearTest() throws DataAccessException {
+        userDAO.createUser(userData);
+        gameDAO.createGame(gameOne);
+        authDAO.createAuth(userData.username());
+        userDAO.deleteAllUsers();
+        gameDAO.deleteAllGames();
+        authDAO.deleteAllAuth();
+    }
+
+    @Test
+    public void positiveCreateUser() throws DataAccessException{
+        userDAO.createUser(userData);
+    }
+
+    @Test
+    public void positiveReadUser() throws DataAccessException{
+        userDAO.createUser(userData);
+        UserData resUser = userDAO.readUser(userData.username());
+    }
+
+    @Test
+    public void positiveDeleteAllUsers() throws DataAccessException{
+        userDAO.createUser(userData);
+        userDAO.deleteAllUsers();
+    }
+
+    @Test
+    public void positiveCreateGame() throws DataAccessException{
+        gameDAO.createGame(gameOne);
+    }
+
+    @Test
+    public void positiveReadGame() throws DataAccessException{
+        gameDAO.createGame(gameOne);
+        GameData resGame = gameDAO.readGame(gameOne.gameID());
+        assertEquals(gameOne, resGame);
+    }
+
+    @Test
+    public void positiveDeleteGame() throws DataAccessException{
+        gameDAO.createGame(gameOne);
+        gameDAO.deleteGame(gameOne.gameID());
+    }
+
+    @Test
+    public void positiveReadAllGames() throws DataAccessException{
+        gameDAO.createGame(gameOne);
+        gameDAO.createGame(gameTwo);
+        gameDAO.createGame(gameThree);
+        ArrayList<GameData> resGameList = gameDAO.readAllGames();
+        ArrayList<GameData> gameList = new ArrayList<>();
+        gameList.add(gameOne);
+        gameList.add(gameTwo);
+        gameList.add(gameThree);
+        assertEquals(gameList,resGameList);
+    }
+
+    @Test
+    public void positiveDeleteAllGames() throws DataAccessException{
+        gameDAO.createGame(gameOne);
+        gameDAO.deleteAllGames();
+    }
+
+    @Test
+    public void positiveCreateAuth() throws DataAccessException{
+        authDAO.createAuth(userData.username());
+    }
+
+    @Test
+    public void positiveReadAuth() throws DataAccessException{
+        String authToken = authDAO.createAuth(userData.username());
+        AuthData authData = authDAO.readAuth(authToken);
+        assertEquals(userData.username(), authData.username());
+    }
+
+    @Test
+    public void positiveDeleteAuth() throws DataAccessException{
+        String authToken = authDAO.createAuth(userData.username());
+        authDAO.deleteAuth(authToken);
+    }
+
+    @Test
+    public void positiveDeleteAllAuth() throws DataAccessException{
+        authDAO.createAuth(userData.username());
+        authDAO.deleteAllAuth();
+    }
+}
